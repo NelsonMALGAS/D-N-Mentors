1265dea6a2a6e5e94ff6b25457109cf15fef6ea0
Nelson Malgas <zmalgas69@gmail.com>
2024-08-07 19:22:08 +0200

    added firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,11 @@
-"use client";
 
-import PwaModal from "@/components/PwaModal";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
-export interface PwaModalProps {
-  onInstall: () => void;
-  onClose: () => void;
-  show: boolean;
-}
+import PwaModalClient from "../components/PwaClient";
 
 export default function Home() {
-  const [show, setShow] = useState(false);
-  const [prompt, setPrompt] = useState<any>(null);
-
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (e: { preventDefault: () => void }) => {
-      e.preventDefault();
-      setPrompt(e);
-
-      if (!window.matchMedia("(display-mode: standalone").matches) {
-        setShow(true);
-      }
-    };
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-
-    return () => {
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt
-      );
-    };
-  }, []);
-
-  const handleInstall = () => {
-    if (prompt) {
-      prompt.prompt();
-
-      prompt.userChoice.then((choiceResult: any) => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("Accepted");
-        } else {
-          console.log("Cancelled");
-        }
-        setPrompt(null);
-        setShow(false);
-      });
-    }
-  };
-  const handleClose = () => {
-    setShow(false);
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-cyan-500">Doctor & Nelson Tutor Mentor Organization</h1>
-      <PwaModal onInstall={handleInstall} onClose={handleClose} show={show} />
+      <PwaModalClient />
     </main>
   );
 }
---
+++ components/PwaClient.tsx
@@ -0,0 +1,58 @@
+"use client"
+import PwaModal from "@/components/PwaModal";
+import { useEffect, useState } from "react";
+
+export interface PwaModalProps {
+  onInstall: () => void;
+  onClose: () => void;
+  show: boolean;
+}
+
+export default function PwaModalClient() {
+  const [show, setShow] = useState(false);
+  const [prompt, setPrompt] = useState<any>(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: { preventDefault: () => void }) => {
+      e.preventDefault();
+      setPrompt(e);
+
+      if (!window.matchMedia("(display-mode: standalone").matches) {
+        setShow(true);
+      }
+    };
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstall = () => {
+    if (prompt) {
+      prompt.prompt();
+
+      prompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("Accepted");
+        } else {
+          console.log("Cancelled");
+        }
+        setPrompt(null);
+        setShow(false);
+      });
+    }
+  };
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <PwaModal onInstall={handleInstall} onClose={handleClose} show={show} />
+    </main>
+  );
+}
