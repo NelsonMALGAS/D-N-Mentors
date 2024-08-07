9cfa9dd2de16aa01582086ef25307716149f80e8
Nelson Malgas <zmalgas69@gmail.com>
2024-08-07 17:47:58 +0200

    added content

--- app/page.tsx
+++ app/page.tsx
@@ -54,6 +54,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-cyan-500">Doctor & Nelson Tutor Mentor Organization</h1>
       <PwaModal onInstall={handleInstall} onClose={handleClose} show={show} />
     </main>
   );
